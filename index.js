f85de0ee90f88559dd084e8d858a63651ae2b248
Attoukora Ange <attoukoraange@gmail.com>
2022-11-26 10:42:14 +0000

    first cors origin

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const server = http.createServer(app);
 
 const option = {
   
-  origin: "https://front-social-beta.vercel.app",
+  origin: process.env.CLIENT,
   methods: ["GET", "PUT", "PATCH" , "POST", "DELETE"],
   allowedHeaders: ["Content-Type", "access_token"],
   credentials: true,
