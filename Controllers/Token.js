2c74b7e2f96a4930888dafafd824007b79dfd265
Attoukora Ange <attoukoraange@gmail.com>
2022-11-27 13:48:47 +0000

    first Authorization Bearer api

--- Controllers/Token.js
+++ Controllers/Token.js
@@ -8,7 +8,7 @@ const createToken = (payload) => {
 const verifieToken = (req, res, next) => {
   // const payload = req.cookies?.access_token;
   
-  const payload = req.headers['authorization'] && req.headers['authorization'].split('Bearer ')[0];
+  const payload = req.headers['authorization'] && req.headers['authorization'].split(' ')[1];
   if (!payload) {
     console.log('pas de token')
     // return res.status(500).json( {SESSION_EXPIRE : "Veuillez vous connecté"});
@@ -29,7 +29,7 @@ const verifieToken = (req, res, next) => {
 
 const verifieTokenJWT = (req, res, next) => {
   // const payload = req.cookies?.access_token;
-  const payload = req.headers['authorization'] && req.headers['authorization'].split('Bearer ')[0];
+  const payload = req.headers['authorization'] && req.headers['authorization'].split(' ')[1];
   console.log('payload ' + payload)
   if (!payload) {
     console.log('pas token')
